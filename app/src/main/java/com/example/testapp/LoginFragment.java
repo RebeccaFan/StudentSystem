ab25087433d288ee91a926164dc670b624f20917
Rebecca Fan <Rebeccasheekfan@gmail.com>
2020-01-31 23:06:28 +0000

    Added login post

--- app/src/main/java/com/example/testapp/LoginFragment.java
+++ app/src/main/java/com/example/testapp/LoginFragment.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -24,8 +26,17 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        Button button = (Button) view.findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Do the login here", Toast.LENGTH_LONG).show();
+            }
+
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
     }
 
 }
